845839be4ba606fac93db2d88760bae776ee4281
Ben Muirhead <bencmuirhead@gmail.com>
2017-01-31 15:48:04 -0500

    created a maze making program

--- data/iss_location/sketch.js
+++ data/iss_location/sketch.js
@@ -7,13 +7,13 @@ var issX
 var issY
 
 function setup() {
-  createCanvas(300, 300)
+  createCanvas(500, 400)
 
-  setInterval(askIss, 1000)
-  
+  setInterval(askIss, 2000)
+
 }
 
-function askIss(){
+function askIss() {
   loadJSON(url, gotData);
 }
 
@@ -21,23 +21,36 @@ function draw() {
   background(0)
   stroke(255)
   line(x, 0, x, height)
-  x = x + 4
+  x = x + 2
   if (x > width) {
     x = 0
   }
   fill(255)
   if (spaceData) {
-  ellipse(issX, issY + 150, 20, 20)
-    }
+    ellipse(issX, issY, 10, 10)
+  }
 }
 
 function gotData(data) {
   print(data)
-  spaceData=data
+  spaceData = data
   var lat = data.iss_position.latitude
-  var long = data.iss_position.longitude
-  issX = map(lat, -180, 180, 0, width)
-  issY = map(long, -180, 180, 0, height)
+  var long = data.iss_position.longitude //* -1
+  print(lat)
+  print(long)
+  lowX = (round(lat/10, -1)*10 - 10)
+  highX = (round(lat/10, -1)*10 + 10)
+  lowY = (round(long/10, -1)*10 - 10)
+  highY = (round(long/10, -1)*10 + 10)
+  print(lowX)
+  print(highX)
+  print(lowY)
+  print(highY)
+  issX = map(lat, lowX, highX, 0, width)
+  issY = map(long, lowY, highY, 0, height)
+
+  // issX = map(lat, -180, 180, 0, width)
+  // issY = map(long, -180, 180, 0, height)
   print(issX)
   print(issY)
 }
